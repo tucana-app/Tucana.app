adf3c4892409e797ed4cdff906f82856562bc29a
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2021-03-08 14:20:25 -0600

    Adding packages

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+require("dotenv").config();
+const { Client } = require("pg");
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
 
 // API KEYS
 
-const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+const REACT_APP_GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+console.log(REACT_APP_GOOGLE_MAPS_API_KEY);
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -19,28 +23,55 @@ if (!isDev && cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
   // Answer API requests.
-  app.get('/get-maps-api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send(`{"mapsApiKey":"${GOOGLE_MAPS_API_KEY}"}`);
+  app.get("/get-maps-api", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send(`{"mapsApiKey":"${REACT_APP_GOOGLE_MAPS_API_KEY}"}`);
   });
 
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  client.connect();
+
+  client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    }
+  );
+
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
   });
 
   app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
   });
 }
