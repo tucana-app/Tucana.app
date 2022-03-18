fb91d05a6085964bee9b540231e25c0b4db5a96a
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2022-03-17 21:17:57 -0600

    Donate links

--- react-ui/src/containers/Donate.js
+++ react-ui/src/containers/Donate.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDollarSign,
+  faHandHoldingUsd,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBitcoin } from "@fortawesome/free-brands-svg-icons";
 
 const Donate = () => {
   return (
@@ -10,36 +16,44 @@ const Donate = () => {
           <p className="lead">
             Thank you so much for your consideration
             <br />
-            We need funds to hire more people and improve our platform
+            We are in search for funds to hire more people and improve our
+            platform
           </p>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col className="text-center">
+          <a
+            href="https://giveth.io/project/Carpooling-App-in-Costa-Rica-0"
+            target={"blank"}
+            className="me-3"
+          >
+            <Button variant="warning" className="rounded-0">
+              <FontAwesomeIcon icon={faBitcoin} className="me-2" />
+              Giveth.io
+            </Button>
+          </a>
+
+          <a
+            href="https://www.paypal.com/donate/?hosted_button_id=M4QRJF5GDHCKA"
+            target={"blank"}
+            className="me-3"
+          >
+            <Button variant="success" className="rounded-0">
+              <FontAwesomeIcon icon={faDollarSign} className="me-2" />
+              PayPal (donation)
+            </Button>
+          </a>
         </Col>
       </Row>
       <Row>
         <Col className="text-center">
-          <form
-            action="https://www.paypal.com/donate"
-            method="post"
-            target="_top"
-          >
-            <input type="hidden" name="business" value="DAA9LC3MG6KDQ" />
-            <input type="hidden" name="no_recurring" value="0" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-              border="0"
-              name="submit"
-              title="PayPal - The safer, easier way to pay online!"
-              alt="Donate with PayPal button"
-            />
-            <img
-              alt=""
-              border="0"
-              src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-              width="1"
-              height="1"
-            />
-          </form>
+          <a href="http://ridecr.atwebpages.com" target={"blank"}>
+            <Button variant="outline-success" className="rounded-0" size={"lg"}>
+              <FontAwesomeIcon icon={faHandHoldingUsd} className="me-2" />
+              Crowdfunding website
+            </Button>
+          </a>
         </Col>
       </Row>
     </Container>
