b47986d8e820d5703f3b341ca13d3afee7c70a11
benjamin.jaume <47286801+BenjaminJaume@users.noreply.github.com>
2022-09-19 08:57:27 -0600

    Editing UI confirm ride

--- src/containers/RidesToConfirm.js
+++ src/containers/RidesToConfirm.js
@@ -4,7 +4,12 @@ import { Redirect, Link } from "react-router-dom";
 import { Container, Row, Col, ListGroup, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useTranslation } from "react-i18next";
-import { ChevronRightIcon, DotFillIcon } from "@primer/octicons-react";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ChevronRightIcon,
+  DotFillIcon,
+} from "@primer/octicons-react";
 import dateFormat from "dateformat";
 
 import LoadingSpinner from "../components/LoadingSpinner";
@@ -51,28 +56,44 @@ function RidesToConfirm() {
           </Row>
         ) : ridesToConfirmData.length ? (
           <Row>
-            <Col xs={12} sm={10} md={8} lg={6} xl={4} className="mx-auto">
-              <ListGroup variant="flush" className="pt-4">
+            <Col xs={12} sm={10} md={8} lg={6} xl={4} className="px-0">
+              <ListGroup variant="flush" className="mx-0 pt-4">
                 {ridesToConfirmData.map((ride, index) => (
                   <Link
                     key={index}
                     to={`/ride/${ride.id}`}
                     className="text-decoration-none"
                   >
-                    <ListGroup.Item className="border border-start-0 border-end-0 ">
-                      <div className="d-inline-flex justify-content-between w-100 py-2">
-                        <span>
-                          <DotFillIcon
-                            size={24}
-                            className="text-success me-2"
-                          />
-                          {ride.origin.city} - {ride.destination.city}{" "}
-                          <small>
-                            ({dateFormat(ride.dateTimeOrigin, "dd/mm/yyyy")})
-                          </small>
-                        </span>
-                        <ChevronRightIcon size={24} verticalAlign="middle" />
-                      </div>
+                    <ListGroup.Item className="border border-start-0 border-end-0">
+                      <Container className="mx-0 px-0">
+                        <Row className="align-items-center">
+                          <Col xs={1} className="mx-0 px-0">
+                            <span>
+                              <DotFillIcon
+                                size={24}
+                                className="text-success me-2"
+                              />
+                            </span>
+                          </Col>
+                          <Col className="ps-0">
+                            <p className="mb-0">
+                              {ride.origin.city}{" "}
+                              <ArrowRightIcon verticalAlign="middle" />{" "}
+                              {ride.destination.city}{" "}
+                            </p>
+                            <small>
+                              {t("translation:global.date")}:{" "}
+                              {dateFormat(ride.dateTimeOrigin, "dd/mm/yyyy")}
+                            </small>
+                          </Col>
+                          <Col xs={1} className="mx-0 px-0">
+                            <ChevronRightIcon
+                              size={24}
+                              verticalAlign="middle"
+                            />
+                          </Col>
+                        </Row>
+                      </Container>
                     </ListGroup.Item>
                   </Link>
                 ))}
